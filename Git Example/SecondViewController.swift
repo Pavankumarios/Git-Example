858c30bf1b4bebdd6d0d4a325aacd60598fbbf0f
Pavankumarios <kvpavan4481@gmail.com>
2022-12-08 14:35:24 +0530

    Added Button Action

--- Git Example/SecondViewController.swift
+++ Git Example/SecondViewController.swift
@@ -11,6 +11,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+         view.backgroundColor = .systemCyan
          print("Second View Controller")
     }
 }
--- Git Example/ViewController.swift
+++ Git Example/ViewController.swift
@@ -8,16 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var nextButton: UIButton!
+    
+    @IBOutlet weak var nextButton   : UIButton!
     @IBOutlet weak var testLabel    : UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         testLabel.text = "Welcome to Git Commit"
     }
     
     @IBAction func buttonAction(_ sender: Any) {
-        
+        let screenObj = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(screenObj, animated: true)
     }
+    
 }
-
