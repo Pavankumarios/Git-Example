44a7893a78cc20cf45a9d3e0a5013800c9053c3d
Pavankumarios <kvpavan4481@gmail.com>
2022-12-08 14:26:18 +0530

    Added Second View Controller

---
+++ Git Example/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  Git Example
+//
+//  Created by PranaviM on 08/12/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print("Second View Controller")
+    }
+}
--- Git Example/ViewController.swift
+++ Git Example/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var testLabel    : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         testLabel.text = "Welcome to Git Commit"
     }
-
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        
+    }
 }
 
