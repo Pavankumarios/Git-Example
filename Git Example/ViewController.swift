b361989389a89c722318a17c131a5fc2be295ab9
Pavankumarios <kvpavan4481@gmail.com>
2022-12-08 15:04:53 +0530

    Added text

--- Git Example/ViewController.swift
+++ Git Example/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        testLabel.text = "welcome
+        testLabel.text = "welcome....."
         
     }
     
